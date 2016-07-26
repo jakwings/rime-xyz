8696fd3099f4beb845133666cecf960a26eb47f6
Jak Wings <jakwings@gmail.com>
2016-07-26 21:56:38 +0800

    opencc: 更新腳本
    將異體字納入繁簡轉換。

--- opencc/compile.sh
+++ opencc/compile.sh
@@ -1,13 +1,52 @@
 #!/usr/bin/env bash
 
+set -e
+
+export LC_ALL=C
+
+compile() {
+  input=$1
+  output=$2
+  cut -d $'\t' -f1-2 $input | \
+    opencc_dict -f text -t ocd -i /dev/stdin -o $output
+}
+
 V_SCRIPT_PATH=$(dirname $0)
 cd $V_SCRIPT_PATH
 
-for input in *.txt
+compile t2s-char.txt t2s-char.ocd
+compile t2s-word.txt t2s-word.ocd
+compile v2s-char.txt v2s-char.ocd
+
+for src in *.txt
 do
-  output=${input%.txt}.ocd
-  cut -d $'\t' -f1-2 $input | \
-    opencc_dict -f text -t ocd -i /dev/stdin -o $output \
-      && echo "DONE: $input to $output" \
-      || echo "FAILED: $input to $output"
+  cp $src ${src%.txt}.tsv
+done
+
+>> t2s-char.tsv awk '
+  BEGIN { FS=OFS="\t" }
+  NF == 1 { t[FNR] = $1; next }
+  NF > 1 { print $1, dedup(t[FNR] " " $2) }
+
+  function dedup(chars, n,t,s,sep) {
+    n = split(chars, a, " ")
+    for (i = 1; i <= n; i++) {
+      if (!t[a[i]]) {
+        s = s sep a[i]
+        t[a[i]] = 1
+      }
+      if (i == 1) sep = " "
+    }
+    return s
+  }
+' <(cut -d $'\t' -f2 v2s-char.txt | opencc -c t2s.json) \
+  <(cut -d $'\t' -f1-2 v2s-char.txt)
+
+for input in *.tsv
+do
+  output=${input%.tsv}.ocd
+  compile $input $output \
+    && echo "DONE: $input to $output" \
+    || echo "FAILED: $input to $output"
+  rm $input
 done
